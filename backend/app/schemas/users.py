116d238b7a074118cd3d891ad0651f63ac556d32
jasonjaeil <83194187+jasonjaeil@users.noreply.github.com>
2025-02-11 14:51:23 +0900

    Update users.py

--- backend/app/schemas/users.py
+++ backend/app/schemas/users.py
@@ -1,14 +1,1 @@
-from pydantic import BaseModel
 
-class UserSignup(BaseModel):
-    name: str
-    user_id: str
-    password: str
-    
-class UserLogin(BaseModel):
-    user_id: str
-    password: str
-
-# 추가사항
-class PasswordRecovery(BaseModel):
-    email: str
